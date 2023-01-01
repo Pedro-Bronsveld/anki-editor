bc624ea94dd468ea567491f9fb19f6452abb0bb6
Pedro Bronsveld <pedrobronsveld@gmail.com>
2023-01-01 15:45:14 +0100

    Add autoanki anki-connect package

--- src/extension.ts
+++ src/extension.ts
@@ -1,3 +1,5 @@
+import { invoke } from '@autoanki/anki-connect';
+
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
@@ -17,6 +19,18 @@ export function activate(context: vscode.ExtensionContext) {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
 		vscode.window.showInformationMessage('Hello World from anki-template-editor!');
+
+		invoke({
+			action: "modelNames",
+			version: 6,
+			request: undefined
+		}).then(res => {
+			console.log(res);
+		}).catch(err => {
+			console.log(err);
+		});
+		
+		
 	});
 
 	context.subscriptions.push(disposable);
