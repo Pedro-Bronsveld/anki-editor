f9edc884d2abd9b5f7e6a856e968545d4f6eb157
Pedro Bronsveld <pedrobronsveld@gmail.com>
2023-02-06 01:29:57 +0100

    Add basic css diagnostics

--- src/language-service/template-diagnostics-collection.ts
+++ src/language-service/template-diagnostics-collection.ts
@@ -1,15 +1,27 @@
 import * as vscode from 'vscode';
 import LanguageFeatureProviderBase from './language-feature-provider-base';
-import { getCSSLanguageService } from 'vscode-css-languageservice';
+import { getCSSLanguageService, TextDocument as CssTextDocument } from 'vscode-css-languageservice';
 
 export default class TemplateDiagnosticsProvider extends LanguageFeatureProviderBase {
 
     async updateDiagnostics(document: vscode.TextDocument, collection: vscode.DiagnosticCollection): Promise<void> {
+        if (document.languageId !== "anki") {
+            collection.clear();
+            return;
+        }
+        
         const embeddedDocument = super.getEmbeddedByLanguage(document, "css");
+
+        if (!embeddedDocument)
+            return;
         
-        // const cssLanguageService = getCSSLanguageService();
-        
-        // cssLanguageService.doValidation(document, );
+        const cssLanguageService = getCSSLanguageService();
+
+        const cssDocument = await vscode.workspace.openTextDocument(embeddedDocument.virtualUri);
+        const stylesheet = cssLanguageService.parseStylesheet(cssDocument as unknown as CssTextDocument);
+        const diagnostics = cssLanguageService.doValidation(cssDocument as unknown as CssTextDocument, stylesheet);
+
+        collection.set(document.uri, diagnostics as unknown as vscode.Diagnostic[]);
 
         return;
     }
