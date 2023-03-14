d5a2092b775af46a9fbcffb7e9e9cc80ca0bbe7e
Pedro Bronsveld <pedrobronsveld@gmail.com>
2023-03-14 20:02:16 +0100

    Add clear cache method with predicate

--- src/utility/cached-function.ts
+++ src/utility/cached-function.ts
@@ -1,35 +1,53 @@
 type AnyFunction = (...args: any[]) => any;
 
-export type CachedFunction<Func extends AnyFunction> = ((...args: Parameters<Func>) => ReturnType<Func>) & {
+type CachedEntry<Func extends AnyFunction> = {
+    args: Parameters<Func>,
+    value: ReturnType<Func>
+}
+
+export type CachedFunction<Func extends AnyFunction> = ((...args: Parameters<Func>) => ReturnType<Func>) & CachedFunctionMethods<Func>;
+
+type CachedFunctionMethods<Func extends AnyFunction> =
+{
     clearCache: () => void;
+    clearCacheWhere: (predicate: (entry: CachedEntry<Func>, key: string) => boolean) => void;
 }
 
 export const createCachedFunction = <Func extends AnyFunction>(func: Func): CachedFunction<Func> => {
 
-    const cache = new Map<string, ReturnType<Func>>();
+    const cache = new Map<string, CachedEntry<Func>>();
     
     return Object.assign((...args: Parameters<Func>) => {
 
         const key = args.join(",");
         const cachedResult = cache.get(key);
         if (cachedResult) {
-            return cachedResult;
+            return cachedResult.value;
         }
 
         const result = func(...args);
+
+        const entry: CachedEntry<Func> = {
+            args,
+            value: result
+        }
+        cache.set(key, entry);
 
         if (result instanceof Promise)
             result.catch(err => {
                 cache.delete(key);
                 throw err;
             });
-        
-        cache.set(key, result);
-        
+                
         return result;
-    }, {
+    }, <CachedFunctionMethods<Func>>{
         clearCache: () => {
             cache.clear();
+        },
+        clearCacheWhere: (predicate) => {
+            [...cache]
+                .filter(([key, entry]) => predicate(entry, key))
+                .forEach(([key, _]) => cache.delete(key));
         }
     });
 }
