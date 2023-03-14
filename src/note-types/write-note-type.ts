1bb2ae2fc2f06b73266627f5d74f18e43a9ed9cf
Pedro Bronsveld <pedrobronsveld@gmail.com>
2023-03-14 20:06:48 +0100

    Replace write note-type functions with anki-connect methods

--- src/note-types/write-note-type.ts
+++ src/note-types/write-note-type.ts
@@ -1,11 +1,10 @@
 import { TextDecoder } from "util";
 import * as vscode from "vscode";
-import { updateModelStyling } from "../anki-connect/update-model-styling";
-import { updateModelTemplates } from "../anki-connect/update-model-templates";
+import { AnkiConnect } from "../anki-connect/anki-connect";
 import { TEMPLATE_EXTENSION } from "../constants";
 import { uriPathToParts } from "./uri-parser";
 
-export const writeNoteType = (uri: vscode.Uri, content: Uint8Array): Promise<void> => {
+export const writeNoteType = (uri: vscode.Uri, content: Uint8Array, ankiConnect: AnkiConnect): Promise<void> => {
 
     const parts = uriPathToParts(uri).slice(1);
     if (parts.length === 0)
@@ -16,7 +15,7 @@ export const writeNoteType = (uri: vscode.Uri, content: Uint8Array): Promise<voi
 
     if (parts.length === 2 && cardName === "Styling.css")
         // save style
-        return updateModelStyling(modelName, decodedContent);
+        return ankiConnect.updateModelStyling(modelName, decodedContent);
     else if (parts.length === 3 && sideFileName.endsWith(`${TEMPLATE_EXTENSION}`)) {
         // save template
         const side = sideFileName.split(".")[0];
@@ -24,7 +23,7 @@ export const writeNoteType = (uri: vscode.Uri, content: Uint8Array): Promise<voi
         if (side !== "Front" && side !== "Back")
             throw vscode.FileSystemError.FileNotFound(uri);
         
-        return updateModelTemplates(modelName, cardName, side, decodedContent);
+        return ankiConnect.updateModelTemplates(modelName, cardName, side, decodedContent);
     }
         
     throw vscode.FileSystemError.FileNotFound(`Writing to ${uri} is not supported by this extension.`);
