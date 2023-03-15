dfa30184650a9e5197237d89a15c067ab8eb665f
Pedro Bronsveld <pedrobronsveld@gmail.com>
2023-03-15 20:48:12 +0100

    Move embedded methods to virtual document provider

--- src/language-service/feature-providers/language-feature-provider-base.ts
+++ src/language-service/feature-providers/language-feature-provider-base.ts
@@ -1,6 +1,6 @@
 import * as vscode from 'vscode';
 import { combineLanguageRegionsById, createVirtualUri, defaultLanguageRegion, getLanguageRegions, LanguageRegion } from '../language-regions';
-import VirtualDocumentProvider from "../virtual-documents-provider";
+import VirtualDocumentProvider, { EmbeddedDocument } from "../virtual-documents-provider";
 
 export default abstract class LanguageFeatureProviderBase {
 
@@ -16,46 +16,11 @@ export default abstract class LanguageFeatureProviderBase {
      * @memberof TemplateBaseProvider
      */
     protected getEmbeddedByPosition(document: vscode.TextDocument, position: vscode.Position): EmbeddedDocument {
-        const languageRegions = getLanguageRegions(document);
-        const offset = document.offsetAt(position);
-    
-        const positionRegion = languageRegions.find(region => offset >= region.start && offset <= region.end)
-    
-        if (!positionRegion)
-            return this.toEmbeddedDocument(defaultLanguageRegion(document), document.uri);
-    
-        const resultRegion = combineLanguageRegionsById(languageRegions).find(region => region.languageId === positionRegion.languageId);
-        
-        return this.toEmbeddedDocument(resultRegion ?? defaultLanguageRegion(document), document.uri);
+        return this.virtualDocumentProvider.getEmbeddedByPosition(document, position);
     }
 
     protected getEmbeddedByLanguage(document: vscode.TextDocument, languageId: string): EmbeddedDocument | undefined {
-        const languageRegions = getLanguageRegions(document);
-
-        const combinedLanguageRegions = combineLanguageRegionsById(languageRegions);
-
-        const languageRegion = combinedLanguageRegions.find(region => region.languageId === languageId);
-
-        if (!languageRegion)
-            return undefined;
-
-        return this.toEmbeddedDocument(languageRegion, document.uri);
-    }
-
-    private toEmbeddedDocument(region: LanguageRegion, originalUri: vscode.Uri): EmbeddedDocument {
-        const virtualUri = createVirtualUri(region.languageId, region.fileExtension, originalUri);
-        this.virtualDocumentProvider.setDocumentContent(virtualUri, region.content);
-        return {
-            content: region.content,
-            languageId: region.languageId,
-            virtualUri
-        };
+        return this.virtualDocumentProvider.getEmbeddedByLanguage(document, languageId);
     }
     
-}
-
-export type EmbeddedDocument = {
-    languageId: string,
-    content: string,
-    virtualUri: vscode.Uri
 }
--- src/language-service/virtual-documents-provider.ts
+++ src/language-service/virtual-documents-provider.ts
@@ -1,4 +1,5 @@
 import * as vscode from 'vscode';
+import { combineLanguageRegionsById, createVirtualUri, defaultLanguageRegion, getLanguageRegions, LanguageRegion } from './language-regions';
 
 export default class VirtualDocumentProvider implements vscode.TextDocumentContentProvider {
     private documents = new Map<string, string>();
@@ -17,4 +18,55 @@ export default class VirtualDocumentProvider implements vscode.TextDocumentConte
         this.documents.clear();
     };
 
+    /**
+     * Gets the embedded contents of a given document at a position and stores it in the VirtualDocumentProvider.
+     *
+     * @protected
+     * @param {vscode.TextDocument} document
+     * @param {vscode.Position} position
+     * @returns {EmbeddedDocument}
+     * @memberof TemplateBaseProvider
+     */
+    public getEmbeddedByPosition(document: vscode.TextDocument, position: vscode.Position): EmbeddedDocument {
+        const languageRegions = getLanguageRegions(document);
+        const offset = document.offsetAt(position);
+    
+        const positionRegion = languageRegions.find(region => offset >= region.start && offset <= region.end)
+    
+        if (!positionRegion)
+            return this.toEmbeddedDocument(defaultLanguageRegion(document), document.uri);
+    
+        const resultRegion = combineLanguageRegionsById(languageRegions).find(region => region.languageId === positionRegion.languageId);
+        
+        return this.toEmbeddedDocument(resultRegion ?? defaultLanguageRegion(document), document.uri);
+    }
+
+    public getEmbeddedByLanguage(document: vscode.TextDocument, languageId: string): EmbeddedDocument | undefined {
+        const languageRegions = getLanguageRegions(document);
+
+        const combinedLanguageRegions = combineLanguageRegionsById(languageRegions);
+
+        const languageRegion = combinedLanguageRegions.find(region => region.languageId === languageId);
+
+        if (!languageRegion)
+            return undefined;
+
+        return this.toEmbeddedDocument(languageRegion, document.uri);
+    }
+
+    private toEmbeddedDocument(region: LanguageRegion, originalUri: vscode.Uri): EmbeddedDocument {
+        const virtualUri = createVirtualUri(region.languageId, region.fileExtension, originalUri);
+        this.setDocumentContent(virtualUri, region.content);
+        return {
+            content: region.content,
+            languageId: region.languageId,
+            virtualUri
+        };
+    }
+}
+
+export type EmbeddedDocument = {
+    languageId: string,
+    content: string,
+    virtualUri: vscode.Uri
 }
