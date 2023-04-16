956d6d71f28e2d3fa45c759487ccef42cf08bf1e
Pedro Bronsveld <pedrobronsveld@gmail.com>
2023-04-16 04:51:39 +0200

    Update permission function request header

--- src/anki-connect/request-permission.ts
+++ src/anki-connect/request-permission.ts
@@ -1,10 +1,29 @@
 import { InvokeResponse } from "@autoanki/anki-connect";
 import { wrappedInvoke } from "./wrapper-invoke";
+import axios, { AxiosResponse } from "axios";
 
-export const requestPermission = async (origin?: string) =>
-    await wrappedInvoke({
+type RequestPermissionRequestBody = {
+    action: "requestPermission",
+    version: 6
+}
+
+type RequestPermissionResponseBody = {
+    result: {
+        permission: "granted",
+        requireApiKey: boolean,
+        version: 6
+    } | {
+        permission: "denied"
+    },
+    error: null
+}
+
+export const requestPermission = async (origin?: string): Promise<RequestPermissionResponseBody["result"]> => 
+    (await axios.post<RequestPermissionResponseBody, AxiosResponse<RequestPermissionResponseBody>, RequestPermissionRequestBody>(origin ?? "http://127.0.0.1:8765", {
         action: "requestPermission",
-        version: 6,
-        request: undefined,
-        origin
-    }) as Omit<InvokeResponse<"requestPermission", 6>, "requireApiKey"> & { requireApikey?: boolean };
+        version: 6
+    },{
+        headers: {
+            "Content-Type": "text/plain"
+        }
+    })).data.result;
